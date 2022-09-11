936da468a1a193444e0e049319ba3e746c398e01
MurkyMeow <dinosowermurky@gmail.com>
2022-09-11 16:08:18 +0300

    fix incorrect quaternion update

--- src/Sensor.cpp
+++ src/Sensor.cpp
@@ -73,7 +73,14 @@ private:
 
     but is much less computationally intensive---it can be performed on a 3.3 V Pro Mini operating at 8 MHz!
   */
-  void MadgwickQuaternionUpdate(float delta_t)
+  void MadgwickQuaternionUpdate(
+      float delta_t,
+      float ax,
+      float ay,
+      float az,
+      float gyrox,
+      float gyroy,
+      float gyroz)
   {
     float q1 = q[0], q2 = q[1], q3 = q[2], q4 = q[3];         // short name local variable for readability
     float norm;                                               // vector norm
@@ -211,26 +218,26 @@ public:
       gyrox = (float)gyroCount[0] * gRes; // get actual gyro value, this depends on scale being set
       gyroy = (float)gyroCount[1] * gRes;
       gyroz = (float)gyroCount[2] * gRes;
-
-      MadgwickQuaternionUpdate(delta_t);
-
-      // Define output variables from updated quaternion---these are Tait-Bryan angles, commonly used in aircraft orientation.
-      // In this coordinate system, the positive z-axis is down toward Earth.
-      // Yaw is the angle between Sensor x-axis and Earth magnetic North (or true North if corrected for local declination, looking down on the sensor positive yaw is counterclockwise.
-      // Pitch is angle between sensor x-axis and Earth ground plane, toward the Earth is positive, up toward the sky is negative.
-      // Roll is angle between sensor y-axis and Earth ground plane, y-axis up is positive roll.
-      // These arise from the definition of the homogeneous rotation matrix constructed from quaternions.
-      // Tait-Bryan angles as well as Euler angles are non-commutative; that is, the get the correct orientation the rotations must be
-      // applied in the correct order which for this configuration is yaw, pitch, and then roll.
-      // For more see http://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles which has additional links.
-      data.yaw = atan2(2.0f * (q[1] * q[2] + q[0] * q[3]), q[0] * q[0] + q[1] * q[1] - q[2] * q[2] - q[3] * q[3]);
-      data.pitch = -asin(2.0f * (q[1] * q[3] - q[0] * q[2]));
-      data.roll = atan2(2.0f * (q[0] * q[1] + q[2] * q[3]), q[0] * q[0] - q[1] * q[1] - q[2] * q[2] + q[3] * q[3]);
-
-      data.pitch *= 180.0f / PI;
-      data.yaw *= 180.0f / PI;
-      data.roll *= 180.0f / PI;
     }
+
+    MadgwickQuaternionUpdate(delta_t, ax, ay, az, gyrox * PI / 180.0f, gyroy * PI / 180.0f, gyroz * PI / 180.0f);
+
+    // Define output variables from updated quaternion---these are Tait-Bryan angles, commonly used in aircraft orientation.
+    // In this coordinate system, the positive z-axis is down toward Earth.
+    // Yaw is the angle between Sensor x-axis and Earth magnetic North (or true North if corrected for local declination, looking down on the sensor positive yaw is counterclockwise.
+    // Pitch is angle between sensor x-axis and Earth ground plane, toward the Earth is positive, up toward the sky is negative.
+    // Roll is angle between sensor y-axis and Earth ground plane, y-axis up is positive roll.
+    // These arise from the definition of the homogeneous rotation matrix constructed from quaternions.
+    // Tait-Bryan angles as well as Euler angles are non-commutative; that is, the get the correct orientation the rotations must be
+    // applied in the correct order which for this configuration is yaw, pitch, and then roll.
+    // For more see http://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles which has additional links.
+    data.yaw = atan2(2.0f * (q[1] * q[2] + q[0] * q[3]), q[0] * q[0] + q[1] * q[1] - q[2] * q[2] - q[3] * q[3]);
+    data.pitch = -asin(2.0f * (q[1] * q[3] - q[0] * q[2]));
+    data.roll = atan2(2.0f * (q[0] * q[1] + q[2] * q[3]), q[0] * q[0] - q[1] * q[1] - q[2] * q[2] + q[3] * q[3]);
+
+    data.pitch *= 180.0f / PI;
+    data.yaw *= 180.0f / PI;
+    data.roll *= 180.0f / PI;
 
     return &data;
   }
